67c621b63d9df1d84021d4db4d6385417d0bb756
Rafael Tosta <rafa.ecomp@gmail.com>
2024-10-08 15:28:18 -0300

    Create vite-project-tailwind-example

---
+++ vite-project-tailwind-example/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
